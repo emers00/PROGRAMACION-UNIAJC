e7a99934187fcb3d25e661ae1231d04b38e65d8c
emers00 <emersonorejuela@estudiante.uniajc.edu.co>
2025-02-03 21:08:59 +0000

    Se agregó UNIAJC al mensaje

--- Ejecutar.java
+++ Ejecutar.java
@@ -1,5 +1,7 @@
 public class Ejecutar{
-    public static void main(String [] args) {
-        System.out.println("Hola Mundo!!!" );
+    public static void main(String [] args){
+
+        System.out.println("Hola Mundo!!! UNIAJC");
     }
+        
 }
